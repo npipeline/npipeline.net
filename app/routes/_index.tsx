6ddfcbb7aeb873e739f30b1d957d0b5d1e7ffa86
Chris Jacques <cmjacques@gmail.com>
2026-04-25 14:47:46 +1000

    feat: compose HomePage with all sections and update route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,5 @@
-import { Container, Title, Text, Button, Stack } from "@mantine/core";
-import { Link } from "react-router";
+import { HomePage } from "~/components/HomePage";
 
-export default function HomePage() {
-  return (
-    <Container size="md" py="xl">
-      <Stack align="center" gap="lg">
-        <Title order={1}>Welcome to NPipeline</Title>
-        <Text size="lg" c="dimmed" ta="center">
-          High-Performance, Type-Safe, Streaming Data Pipelines in .NET{" "}
-        </Text>
-        <Button component={Link} to="/about" size="md">
-          Learn More
-        </Button>
-      </Stack>
-    </Container>
-  );
+export default function Index() {
+  return <HomePage />;
 }
